d2d76faa3275def031afec6444fec704af24ecbb
Toan Nguyen <tnguyen29032002@gmail.com>
2025-11-03 15:09:38 -0700

    Refactor: Add comprehensive JavaDoc documentation to Room and BadConfigFormatException classes

--- src/clueGame/BadConfigFormatException.java
+++ src/clueGame/BadConfigFormatException.java
@@ -1,11 +1,22 @@
 package clueGame;
 
+/**
+ * Custom exception thrown when configuration files have invalid format
+ * This includes setup files and layout files that don't match expected format
+ */
 public class BadConfigFormatException extends Exception {
 	
+	/**
+	 * Default constructor with generic error message
+	 */
 	public BadConfigFormatException() {
 		super("Bad configuration file format detected");
 	}
 	
+	/**
+	 * Constructor with custom error message
+	 * @param message the detailed error message
+	 */
 	public BadConfigFormatException(String message) {
 		super(message);
 	}
